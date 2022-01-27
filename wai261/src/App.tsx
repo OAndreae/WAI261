28c246ac9807d005de6be4fa78e5eb24052dba18
OAndreae <oliverandreae@outlook.com>
2022-01-27 19:04:06 +0000

    Initial commit

---
+++ wai261/src/App.tsx
@@ -0,0 +1,25 @@
+// 1. import `ChakraProvider` component
+import { ChakraProvider, Heading, Center, Input, Box, Button, Text } from '@chakra-ui/react'
+
+import { Message } from "Message.tsx";
+
+function App() {
+  // 2. Use at the root of your app
+  return (
+    <ChakraProvider>
+      <Center>
+        <Box>
+          <Heading>ChakraUI Heading</Heading>
+          <Box backgroundColor="lightblue">
+            <Text>Message 1</Text>
+          </Box>
+          <Input onChange={(e) => console.log()} />
+          <Button backgroundColor="lightGrey" onClick={() => console.log("Button clicked.")}>
+            Send message
+          </Button>
+        </Box>
+      </Center>
+    </ChakraProvider>
+  )
+}
+export default App;
---
+++ wai261/src/Message.tsx
@@ -0,0 +1,11 @@
+import {Box, Text} from "@chakra-ui/react"
+
+function Message () {
+    return (
+        <Box>
+            <Text>
+                Message
+            </Text>
+        </Box>
+    )
+}
