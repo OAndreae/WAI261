e4dcfb203f18a12ed9e0e4e7a9bff3ebce25c3be
OAndreae <oliverandreae@outlook.com>
2022-01-27 19:14:55 +0000

    Moved project files up
    to the parent directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // 1. import `ChakraProvider` component
 import { ChakraProvider, Heading, Center, Input, Box, Button, Text } from '@chakra-ui/react'
 
-import { Message } from "Message.tsx";
+import Message from "./Message";
 
 function App() {
   // 2. Use at the root of your app
@@ -10,9 +10,14 @@ function App() {
       <Center>
         <Box>
           <Heading>ChakraUI Heading</Heading>
+          <Message/>
+          <Message/>
+          <Message/>
+
           <Box backgroundColor="lightblue">
             <Text>Message 1</Text>
           </Box>
+          
           <Input onChange={(e) => console.log()} />
           <Button backgroundColor="lightGrey" onClick={() => console.log("Button clicked.")}>
             Send message
--- src/Message.tsx
+++ src/Message.tsx
@@ -9,3 +9,5 @@ function Message () {
         </Box>
     )
 }
+
+export default Message;
